f536306e03225690e658363ee227df50115c7685
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2025-09-17 08:24:31 +0900

    feat: 初期設定

---
+++ src/main/java/com/example/springboot_todo_lab/SpringbootTodoLabApplication.java
@@ -0,0 +1,12 @@
+package com.example.springboot_todo_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTodoLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTodoLabApplication.class, args);
+	}
+}
